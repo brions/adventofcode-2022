c62d5be069c84ee3e7d0b519b909e1da3a57ee61
Brion Swanson <brion@mail.rit.edu>
2022-12-04 17:33:20 -0500

    Added smaller sample from full input for testing re-wrote most of the logic

--- day3/elf_rucksack.py
+++ day3/elf_rucksack.py
@@ -8,53 +8,40 @@
 #
 
 import fileinput
+from string import ascii_letters
 
 # translation map of ascii values for A-Z and a-z
 #captialRange = [65,90]
 #lowerRange = [97,122]
 
-def parseRucksack(items: str):
-    prioritySets = [set() for s in range(2)]
-    # print(f'prioritySets: {prioritySets}')
-    pocketMax = (len(items) / 2) - 1
-    pos = 0
-
-    for char in items:
-        if ord(char) >= 97 and ord(char) <= 122:
-            # we have a lowercase letter, subtract 96 to get the priority
-            # print(f'found lowercase letter {char} ({ord(char)})')
-            # convert it to its priority and sort it into the correct pocket
-            priorityValue = ord(char) - 96
-            # print(f'adding priority of {char} ({priorityValue}) to pocket {0 if pos < pocketMax else 1}')
-            prioritySets[0].add(priorityValue) if pos < pocketMax else prioritySets[1].add(priorityValue)
-        elif ord(char) >= 65 and ord(char) <= 90:
-            # we have a lowercase letter, subtract 96 to get the priority
-            # print(f'found uppercase letter {char} ({ord(char)})')
-            # convert it to its priority and sort it into the correct pocket
-            priorityValue = ord(char) - 38
-            # print(f'adding priority of {char} ({priorityValue}) to pocket {0 if pos < pocketMax else 1}')
-            prioritySets[0].add(priorityValue) if pos < pocketMax else prioritySets[1].add(priorityValue)
-        pos += 1
-    return prioritySets
+# load file in here stripping newlines
+lines = list(map(lambda line: line.strip(), open('input.txt', mode='r', encoding='utf-8').readlines()))
 
 prioritySum = 0
 
-# load file in here
-for line in fileinput.input('input.txt'):
-    if not line.strip():
-        continue
+for line in lines:
+    pocketMax = len(line) // 2
+    pocketContents = set(line[:pocketMax]), set(line[pocketMax:])
     
-    prioritySets = parseRucksack(line)
+    # find the intersection of the pockets
+    common = pocketContents[0].intersection(pocketContents[1])
+    commonVal = common.pop()
 
-    # print(f'contents of pocket 1: {prioritySets[0]}\ncontents of pocket 2: {prioritySets[1]}')
-    # print(f'sum of pocket 1: {sum(prioritySets[0])}\nsum of pocket 2: {sum(prioritySets[1])}')
-
-    # find the intersection of the prioritySets
-    common = prioritySets[0].intersection(prioritySets[1])
-
-    print(f'Common set: {common}') if len(common) > 0 else print(f'### Nothing in common! ###')
+    # print(f'Pocket sets: {pocketContents}')
+    # print(f'==> Common priority: {ascii_letters.index(commonVal) + 1} ({commonVal})') if commonVal != None else print(f'### Nothing in common! ###')
     
-    # add common to the sum unless there's nothing in common
-    prioritySum += sum(common) if len(common) > 0 else 0
+    prioritySum += ascii_letters.index(commonVal) + 1
     
-print(f'Total sum of common priorities: {prioritySum}')
+print(f'====> Total sum of common priorities: {prioritySum}')
+
+groupSum = 0
+
+for idx in range(0, len(lines), 3):
+    group = lines[idx:idx+3]
+    badgeType = set(group[0]).intersection(group[1]).intersection(group[2])
+    
+    # print(f'badge type for group {idx//3}: {badgeType}')
+    
+    groupSum += ascii_letters.index(badgeType.pop()) + 1
+    
+print(f'====> Total sum of common group priorities: {groupSum}')
